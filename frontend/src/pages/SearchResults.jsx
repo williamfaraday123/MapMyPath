668cbd273aa324b9af11a2d431a56ce5a2a9c4fc
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2025-03-05 18:02:15 +0800

    Update SearchResults.jsx

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -7,10 +7,16 @@ const SearchResults = () => {
     return (
         <Container fluid>
             <Row className="g-0">
-                <Col xs={12} md={4} style={{ overflowY: "auto", minHeight: "50vh" }}>
+                <Col xs={12} md={4} className="overflow-auto vh-100" style={{ maxHeight: "100vh" }}>
                     <ListComponent />
                 </Col>
-                <Col xs={12} md={8} style={{ minHeight: "50vh" }}>
+                {/* Right: Fixed map (only fixed on larger screens) */}
+                <Col xs={12} md={8} className="vh-100 d-md-block d-none position-fixed end-0">
+                    <MapComponent />
+                </Col>
+
+                {/* Separate map for mobile (stacks below list) */}
+                <Col xs={12} className="d-md-none" style={{ height: "50vh" }}>
                     <MapComponent />
                 </Col>
             </Row>
